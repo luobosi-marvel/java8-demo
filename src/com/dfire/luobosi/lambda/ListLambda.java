9f6557e2eeb4d30e12cb00ce2b7d535fa456004e
luobosi <luobosi@2dfire.com>
2017-11-25 16:47:48 +0800

    完善 Lambda 表达式迭代集合

--- src/com/dfire/luobosi/lambda/ListLambda.java
+++ src/com/dfire/luobosi/lambda/ListLambda.java
@@ -14,30 +14,29 @@ import java.util.List;
  */
 public class ListLambda {
 
-    /**
-     * list 集合对象
-     */
+    /** list 集合对象 */
     private List<String> strLists;
-
+    /** 没有参数的构造方法 */
     public ListLambda() {
         this.strLists = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
     }
 
     public static void main(String[] args) {
-
+        ListLambda listLambda = new ListLambda();
+        listLambda.java8Iterator();
     }
 
     /**
      * Java8 迭代集合对象
-     *
-     * @param strLists 字符串集合对象
      */
-    public void java8Iterator(List<String> strLists) {
+    public void java8Iterator() {
         // java8 迭代
         strLists.forEach(n -> System.out.println(n));
         // 使用 Java 8 的方法引用更方便，方法引用由 :: 双冒号操作符表示，
         // 看起来像 C++ 的作用域解析运算符
         strLists.forEach(System.out::println);
     }
+
+
 
 }
