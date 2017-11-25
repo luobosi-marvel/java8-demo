9108ffa8804ad2bfa2e74e79270492049588fe18
luobosi <luobosi@2dfire.com>
2017-11-25 20:05:54 +0800

    将行为参数化的例子

---
+++ src/com/dfire/luobosi/function/BehaviorParameterization.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.dfire.luobosi.function;
+
+/**
+ * BehaviorParameterization
+ * 将行为参数化
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-11-25
+ */
+public class BehaviorParameterization {
+
+}
--- src/com/dfire/luobosi/optional/Account.java
+++ src/com/dfire/luobosi/optional/Account.java
@@ -11,5 +11,44 @@ package com.dfire.luobosi.optional;
  * @since 2017-11-25
  */
 public class Account {
+    /** 用户id */
+    private String accountId;
+    /** 用户名 */
+    private String username;
+    /** 用户密码 */
+    private String password;
+    /** 用户状态 */
+    private String status;
 
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
