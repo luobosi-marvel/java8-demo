db256d49f11278d46b05684705ec3d4800cc61dd
luobosi <luobosi@2dfire.com>
2017-11-25 17:09:28 +0800

    使用 Java8 的 Optional 类

---
+++ src/com/dfire/luobosi/optional/Account.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.dfire.luobosi.optional;
+
+/**
+ * Account
+ * 给 OptionalTset 方法测试的类
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-11-25
+ */
+public class Account {
+
+}
---
+++ src/com/dfire/luobosi/optional/OptionalTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.dfire.luobosi.optional;
+
+/**
+ * OptionalTest
+ * Java8 中的 Optional 方法进行测试，其实就是写些小 demo
+ * Optional 是 Java8 提供优雅解决 NullPointerException 的问题
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-11-25
+ */
+public class OptionalTest {
+
+}
