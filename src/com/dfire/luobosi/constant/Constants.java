fb090ae24005b41f111e4ac7ebb24d73fe807ac8
luobosi <luobosi@2dfire.com>
2017-11-25 17:13:06 +0800

    添加常量

---
+++ src/com/dfire/luobosi/constant/Constants.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.dfire.luobosi.constant;
+
+/**
+ * Constants
+ * 这个是常量类，提供一些常量
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-11-25
+ */
+public class Constants {
+
+    /**
+     * 关于 Account 的一些常量信息
+     */
+    public interface AccountConstans {
+        /** 启用一个用户 */
+        int ENABLED = 0;
+        /** 禁用一个用户 */
+        int DISABLED = 1;
+    }
+
+
+}
